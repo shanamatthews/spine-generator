600a1b13a8ccbac89ccd6ae4871a2e021174ca36
Shana Matthews <shanamatthews@outlook.com>
2020-10-29 19:37:07 +0000

    get to title

--- main.py
+++ main.py
@@ -3,13 +3,23 @@ import xmltodict
 from dotenv import load_dotenv
 import os
 
+from typing import Final
+
 load_dotenv()
 
-api_key = os.getenv("KEY")
+API_KEY = os.getenv("KEY")
+API_VERSION: Final = 2
+USER_ID: Final = '59980712'
+GR_SHELF: Final = 'read'
 
-params = {'v': '2', 'id': '59980712', 'shelf': 'read', 'key':api_key}
+params = {'v': API_VERSION, 'id': USER_ID, 'shelf': GR_SHELF, 'key':API_KEY}
 response = requests.get('https://www.goodreads.com/review/list', params=params)
+print(response.url)
+
 reviews = xmltodict.parse(response.content)
 
+
 reviews = reviews['GoodreadsResponse']['reviews']['review']
-print(reviews[0])
+print(reviews[1]['book']['title_without_series'])
+print(reviews[1]['book']['image_url'])
+
