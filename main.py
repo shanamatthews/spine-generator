35ecf538da539cf2cc62f976b7be3b1cdc32991f
Shana Matthews <shanamatthews@outlook.com>
2020-10-29 21:50:10 +0000

    move # pages, refactor

--- main.py
+++ main.py
@@ -16,35 +16,38 @@ GR_READING_SHELF: Final = 'currently-reading'
 
 NUM_BOOKS: Final = 60
 
-read_params = {'v': API_VERSION, 'id': USER_ID, 'shelf': GR_READ_SHELF, 'key': API_KEY, 'sort': 'date_read', 'per_page': NUM_BOOKS}
-read_response = requests.get('https://www.goodreads.com/review/list', params=read_params)
+def main():
+    read_params = {'v': API_VERSION, 'id': USER_ID, 'shelf': GR_READ_SHELF, 'key': API_KEY, 'sort': 'date_read', 'per_page': NUM_BOOKS}
+    read_response = requests.get('https://www.goodreads.com/review/list', params=read_params)
 
-reading_params = {'v': API_VERSION, 'id': USER_ID, 'shelf': GR_READING_SHELF, 'key': API_KEY, 'sort': 'date_read'}
-reading_response = requests.get('https://www.goodreads.com/review/list', params=reading_params)
+    reading_params = {'v': API_VERSION, 'id': USER_ID, 'shelf': GR_READING_SHELF, 'key': API_KEY, 'sort': 'date_read'}
+    reading_response = requests.get('https://www.goodreads.com/review/list', params=reading_params)
 
-books_read = xmltodict.parse(read_response.content)['GoodreadsResponse']['reviews']['review']
-books_reading = xmltodict.parse(reading_response.content)['GoodreadsResponse']['reviews']['review']
+    books_read = xmltodict.parse(read_response.content)['GoodreadsResponse']['reviews']['review']
+    books_reading = xmltodict.parse(reading_response.content)['GoodreadsResponse']['reviews']['review']
 
-# print(books_read[16])
+    # print(books_read[16])
 
-read_fields = ['Title', 'Date Finished', 'Image URL', 'Num Pages']
+    create_book_csv(books_read, 'recently-read.csv')
+    create_book_csv(books_reading, 'currently-reading.csv', currently_reading=True)
 
-with open('recently-read.csv', 'w') as csvfile:
-    filewriter = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
-    filewriter.writerow(read_fields)
+def create_book_csv(book_array, file_name, currently_reading=False):
+    with open(file_name, 'w') as csvfile:
+        filewriter = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
 
-    for book in books_read:
-        read_at = book['read_at'] if book['read_at'] != None else book['date_updated']
-        book_data = book['book']
-        filewriter.writerow([book_data['title_without_series'], read_at, book_data['image_url'], book_data['num_pages']])
+        headers = ['Title', 'Date', 'NumPages', 'ImageURL']
 
-reading_fields = ['Title', 'Date Started', 'Image URL', 'Num Pages']
+        filewriter.writerow(headers)
 
-with open('currently-reading.csv', 'w') as csvfile:
-    filewriter = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
-    filewriter.writerow(reading_fields)
+        for book in book_array:
+            date = ""
+            if currently_reading:
+                date = book['started_at']
+            else:
+                date = book['read_at'] if book['read_at'] != None else book['date_updated']
 
-    for book in books_reading:
-        read_at = book['started_at']
-        book_data = book['book']
-        filewriter.writerow([book_data['title_without_series'], read_at, book_data['image_url'], book_data['num_pages']])
+            book_data = book['book']
+            filewriter.writerow([book_data['title_without_series'], date, book_data['num_pages'], book_data['image_url']])
+
+if __name__ == "__main__":
+    main()
